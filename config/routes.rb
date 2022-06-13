30c9bfab879bb3b8e728c55d977bd4a9e5d65fc6
Kevin Triplett <kevin@mopacmedia.com>
2022-06-12 20:08:47 -0600

    fix route error (dangling do)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  resource :game, only: [:new, :create] do
+  resource :game, only: [:new, :create]
   resources :users, only: [:new, :create]
   resources :turns, only: [:create]
 
   # Defines the root path route ("/")
-  root to: "turns#create"
+  root to: "game#show"
 end
