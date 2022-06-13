ca5f7b40b241ac69132e4727dd416e0f7d424056
Kevin Triplett <kevin@mopacmedia.com>
2022-06-12 21:53:38 -0600

    update game operation and tests

--- app/controllers/concerns/game/operation/create.rb
+++ app/controllers/concerns/game/operation/create.rb
@@ -1,22 +1,15 @@
 class Game::Operation::Create < Trailblazer::Operation
-    step :extract_params
-    step :validate_for_create?
+    class Validations < Dry::Validation::Contract
+        params do
+            required(:name).filled
+        end
+    end
+
+    step Contract::Validate(constant: Validations.new, key: :game)
     step :create_model
-    step :save
 
-    def extract_params(ctx, params:, **)
-        ctx[:my_params] = params[:game]
-    end
-
-    def validate_for_create?(ctx, my_params:, **)
-        my_params[:name].present?
-    end
-
-    def create_model(ctx, my_params:, **)
-        ctx[:game] = Game.new(**my_params)
-    end
-
-    def save(ctx, **)
+    def create_model(ctx, **)
+        ctx[:game] = Game.new(**ctx["result.contract.default"].to_h)
         ctx[:game].save
     end
 end
--- test/concepts/game/operation_test.rb
+++ test/concepts/game/operation_test.rb
@@ -2,18 +2,31 @@ require 'test_helper'
 require 'spec/spec_helper'
 
 class GameOperationTest < MiniTest::Spec
-    it "Creates {Game} model when given valid attributes" do
-        result = Game::Operation::Create.wtf?(params: {game: {name: "lorem"}})
+  it "Creates {Game} model when given valid attributes" do
+    result = Game::Operation::Create.wtf?(
+      params: {game: {name: "lorem"}},
+      errors: {}
+    )
 
-        assert_equal true, result.success?
+    assert_equal true, result.success?
 
-        model = result[:game]
-        assert_equal "lorem", model.name
-    end
+    model = result[:game]
+    assert_equal "lorem", model.name
 
-    it "Fails with invalid name attribute" do
-        result = Game::Operation::Create.wtf?(params: {game: {name: ""}})
+    assert_equal({}, result["result.contract.default"].errors.to_h)
+  end
 
-        assert_equal false, result.success?
-    end
+  it "Fails with invalid parameters" do
+    result = Game::Operation::Create.wtf?(params: {})
+
+    assert_equal false, result.success?
+    assert_nil(result["result.contract.default"])
+  end
+
+  it "Fails with invalid name attribute" do
+    result = Game::Operation::Create.wtf?(params: {game: {name: ""}})
+
+    assert_equal false, result.success?
+    assert_equal({:name=>["must be filled"]}, result["result.contract.default"].errors.to_h)
+  end
 end
