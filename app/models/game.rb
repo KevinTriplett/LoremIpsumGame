68b07cfb218c9cf25c4cc7e165832794569283f6
Kevin Triplett <kevin@mopacmedia.com>
2022-06-18 23:17:14 -0500

    add assoc dep to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-    has_many :users
-    has_many :turns
+    has_many :users, dependent: :destroy
+    has_many :turns, dependent: :destroy
     has_one :current_player, class_name: "User"
 end
