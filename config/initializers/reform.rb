1cad8a938153ae45e22de420ce7141b27176807a
Kevin Triplett <kevin@mopacmedia.com>
2022-07-13 11:58:08 -0500

    monky patch Reform#full_messages_for

--- config/initializers/reform.rb
+++ config/initializers/reform.rb
@@ -3,3 +3,13 @@ require "reform/form/dry"
 Reform::Form.class_eval do
   include Reform::Form::Dry
 end
+
+# TODO: remove when patched
+class Reform::Contract::Result::Errors
+  def method_missing(method_name, *args)
+    if "full_messages_for" == method_name.to_s
+      param = args[0]
+      @dotted_errors[param].collect { |message| "#{param} #{message}" }
+    end
+  end
+end
