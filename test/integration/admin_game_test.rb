58d674ad62c6b4b1dee249048f36e6241aeaa202
Kevin Triplett <kevin@mopacmedia.com>
2022-07-27 08:45:56 -0400

    fix bizzare test count bug

--- test/integration/admin_game_test.rb
+++ test/integration/admin_game_test.rb
@@ -11,52 +11,31 @@ class AdminGameTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "Admin page with games" do
+  test "Admin page with games and no users" do
     DatabaseCleaner.cleaning do
-      create_game
+      game = create_game
 
       get admin_games_path
       assert_select "h5", "Existing Games"
-      assert_select "span.game-name", last_random_game_name
+      assert_select "span.game-name", game.name
       assert_select "a", "edit"
       assert_select "a", "users"
       assert_select "a", "delete"
       assert_select "a", "New Game"
-    end
-  end
-
-  test "Admin page for game with no users" do
-    DatabaseCleaner.cleaning do
-      game = create_game
 
       get admin_game_users_path(game_id: game.id)
       assert_select "h1", "Lorem Ipsum"
-      assert_select "h5", "No Users for game #{last_random_game_name}"
+      assert_select "h5", "No Users for game #{game.name}"
       assert_select "a", "Add User"
+      assert_select "a", "Back"
     end
   end
 
-  test "Admin page for game with users" do
+  test "Admin page for editing game and editing user" do
     DatabaseCleaner.cleaning do
       game = create_game
       user1 = create_game_user(game.id)
       user2 = create_game_user(game.id)
-
-      get admin_game_users_path(game_id: game.id)
-      assert_select "h1", "Lorem Ipsum"
-      assert_select "h5", "Users for game #{last_random_game_name}"
-      assert_select "a", user1.name
-      assert_select "a", user2.name
-      assert_select "a", "play"
-      assert_select "a", "edit"
-      assert_select "a", "delete"
-      assert_select "a", "Add User"
-    end
-  end
-
-  test "Admin page for game editing" do
-    DatabaseCleaner.cleaning do
-      game = create_game
 
       get edit_admin_game_path(id: game.id)
       assert_select "h1", "Lorem Ipsum"
@@ -64,6 +43,24 @@ class AdminGameTest < ActionDispatch::IntegrationTest
       assert_select "input#game_name[value='#{game.name}']", nil
       assert_select "input#game_game_days[value='#{game.game_days}']", nil
       assert_select "input#game_turn_hours[value='#{game.turn_hours}']", nil
+      assert_select "a", "Cancel"
+
+      get admin_game_users_path(game_id: game.id)
+      assert_select "h1", "Lorem Ipsum"
+      assert_select "h5", "Users for game #{game.name}"
+      assert_select "a", user1.name
+      assert_select "a", user2.name
+      assert_select "a", "play"
+      assert_select "a", "edit"
+      assert_select "a", "delete"
+      assert_select "a", "Add User"
+      assert_select "a", "Back"
+
+      get edit_admin_game_user_path(game_id: game.id, id: user1.id)
+      assert_select "h1", "Lorem Ipsum"
+      assert_select "h5", "Editing user"
+      assert_select "input#user_name[value='#{user1.name}']", nil
+      assert_select "input#user_email[value='#{user1.email}']", nil
     end
   end
 end
