cdda7dad0f889b1725eea878b23ade19e97440bd
Kevin Triplett <kevin@mopacmedia.com>
2022-07-27 08:46:44 -0400

    chrome no work, use firefox

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
+require 'selenium-webdriver'
+require 'webdrivers'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  include Capybara::Minitest::Assertions
+  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
 end
