5491464a2324ad20c4bf1575a284723926b7eefd
Kevin Triplett <kevin@mopacmedia.com>
2022-08-03 14:53:17 -0400

    refactor test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,10 @@ class UserTest < MiniTest::Spec
 
   it "Finds the next user (no rollover)" do
     DatabaseCleaner.cleaning do
-      game = Game.create(name: "game")
-      user1 = User.create(name: "John John", email: "abc@xyz.com", game_id: game.id)
-      user2 = User.create(name: "Jill Jill", email: "cba@xyz.com", game_id: game.id)
-      user3 = User.create(name: "Jack Jack", email: "dbd@xyz.com", game_id: game.id)
+      game = create_game
+      user1 = create_user(game_id: game.id)
+      user2 = create_user(game_id: game.id)
+      user3 = create_user(game_id: game.id)
 
       next_player = User.next_player(user2.id, game.id)
       assert_equal user3.id, next_player.id
@@ -17,10 +17,10 @@ class UserTest < MiniTest::Spec
 
   it "Finds the next user (rollover)" do
     DatabaseCleaner.cleaning do
-      game = Game.create(name: "game")
-      user1 = User.create(name: "John John", email: "abc@xyz.com", game_id: game.id)
-      user2 = User.create(name: "Jill Jill", email: "cba@xyz.com", game_id: game.id)
-      user3 = User.create(name: "Jack Jack", email: "dbd@xyz.com", game_id: game.id)
+      game = create_game
+      user1 = create_user(game_id: game.id)
+      user2 = create_user(game_id: game.id)
+      user3 = create_user(game_id: game.id)
 
       next_player = User.next_player(user3.id, game.id)
       assert_equal user1.id, next_player.id
