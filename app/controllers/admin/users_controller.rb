def811e4e8ad2743e67f3eb441d97985bdad833c
Kevin Triplett <kevin@mopacmedia.com>
2022-08-04 06:10:29 -0400

    refactor

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,20 +12,20 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
-    run User::Operation::Create::Present, game_id: params[:game_id] do |ctx|
+    @game = get_game
+    run User::Operation::Create::Present, game_id: @game.id do |ctx|
       @form = ctx["contract.default"]
-      @game = Game.find(params[:game_id])
       render
     end
   end
 
   def create
-    _ctx = run User::Operation::Create, game_id: params[:game_id] do |ctx|
+    @game = get_game
+    _ctx = run User::Operation::Create, game_id: @game.id do |ctx|
       return redirect_to new_admin_game_user_path
     end
-  
+
     @form = _ctx["contract.default"]
-    @game = Game.find(params[:game_id])
     render :new, status: :unprocessable_entity
   end
 
@@ -34,7 +34,6 @@ class Admin::UsersController < ApplicationController
       @form = ctx["contract.default"]
       render
     end
-  
   end
 
   def update
@@ -52,5 +51,11 @@ class Admin::UsersController < ApplicationController
   
     flash[:notice] = "User deleted"
     redirect_to admin_game_users_path #(game_id: params[:game_id])
+  end
+
+  private
+
+  def get_game
+    Game.find(params[:game_id])
   end
 end
