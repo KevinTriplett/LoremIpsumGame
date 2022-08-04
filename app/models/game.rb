785bcc69d5ced801e2e1fdf62e026fb753745a1f
Kevin Triplett <kevin@mopacmedia.com>
2022-08-03 21:04:00 -0400

    [bug] game returned wrong current player

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,11 @@
 class Game < ActiveRecord::Base
   has_many :users, dependent: :destroy
-  has_one :current_player, class_name: "User"
 
   has_secure_token
+
+  def current_player
+    User.find(current_player_id)
+  end
 
   def last_turn?
     turn_end && game_end && turn_end > game_end
