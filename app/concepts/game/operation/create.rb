130ca78ebe54fd4f17bad2e5bb3c31aae3c7db33
Kevin Triplett <kevin@mopacmedia.com>
2022-08-19 21:33:25 -0500

    refactor pad creation for game creation

--- app/concepts/game/operation/create.rb
+++ app/concepts/game/operation/create.rb
@@ -13,18 +13,14 @@ module Game::Operation
     
     step Subprocess(Present)
     step Contract::Validate(key: :game)
-    step Contract::Persist()
     step :create_pad
+    step Contract::Persist()
 
     def create_pad(ctx, model:, **)
-      begin
-        game = Game.find(model.id)
-        client = EtherpadLite.client(Rails.configuration.etherpad_url, Rails.configuration.etherpad_api_key)
-        nil == client.createPad(padID: game.token, text: Rails.configuration.initial_etherpad_text)
-      rescue
-        puts "ERROR: pad '#{game.token}' could not be created" unless Rails.env == "test"
-        true
-      end
+      return true if Rails.env == "test"
+      game = Game.find(model.id)
+      client = EtherpadLite.client(Rails.configuration.etherpad_url, Rails.configuration.etherpad_api_key)
+      nil == client.createPad(padID: game.token, text: Rails.configuration.initial_etherpad_text)
     end
   end
 end
