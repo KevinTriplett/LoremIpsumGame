1d201edc3ea9272e062252688d532f734a64958c
Kevin Triplett <kevin@mopacmedia.com>
2022-08-20 20:15:11 -0500

    fixup comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    # game is entered through /users/<user_token>/turns/new
+    # game is entered through /users(/:user_token)/turns/new
     return not_found
   end
 end
