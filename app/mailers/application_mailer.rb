2c38924c1935987f202006cfc3236fc11d85f00f
Kevin Triplett <kevin@mopacmedia.com>
2022-09-05 13:21:37 -0600

    add kai to email cc's

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "noreply@loremipsumgame.com"
-  default cc: "kt@kevintriplett.com"
+  default cc: ["kt@kevintriplett.com", "speaktokai@gmail.com"]
   layout "mailer"
 end
