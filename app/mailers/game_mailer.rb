910f57fc8fd030ec057c1045c47eef146ab92622
Kevin Triplett <kevin@mopacmedia.com>
2022-09-28 10:26:46 -0400

    refactor emails

--- app/mailers/game_mailer.rb
+++ app/mailers/game_mailer.rb
@@ -1,9 +1,10 @@
 class GameMailer < ApplicationMailer
   def pause_notification
     @game = params[:game]
+    @url = "https://loremipsumgame.com/admin/games"
     @admins = @game.get_admins.pluck(:email)
     # no admins? send email to users
-    @admins = @game.users.pluck(:email) unless @admins.count > 0
+    @admins = @game.users.pluck(:email) if @admins.count == 0
     mail(to: @admins, subject: "Lorem Ipsum - Game paused")
   end
 end
